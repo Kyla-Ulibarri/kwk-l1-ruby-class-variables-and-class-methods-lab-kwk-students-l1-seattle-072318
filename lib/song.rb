c7ab6dd61740771e95915793a5110aeb7e70126c
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-30 16:20:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,16 @@
-
+class Song
+  
+  @@count = 0 
+  
+  def self.count
+    @@count
+  end
+  
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize
+    @name = name
+    @artist = artist 
+    @genre = genre
+    
+    @@count += 1
